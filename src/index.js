e275b75d8476ad4f8ac61ad1a9843a8b71f449f4
pavs <pavel.semochkin95@gmail.com>
2021-03-08 15:02:18 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,19 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    const joinedBracketsArr = bracketsConfig.map((item) => item.join(""));
+
+    let newStr = str;
+    let isChanged = true;
+
+    while (isChanged) {
+        const replaced = joinedBracketsArr.reduce(
+            (acc, brackets) => acc.replace(brackets, ""),
+            newStr
+        );
+        isChanged = replaced.length !== newStr.length;
+        newStr = replaced;
+
+        if (!newStr) return true;
+    }
+
+    return false;
+};
